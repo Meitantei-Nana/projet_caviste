12654f6f18ea2de4dc30073751697a0b0dad3228
ebru <ebsahinn7887@outlook.com>
2023-10-28 12:19:05 +0200

    ajout du projet

---
+++ projet/apiService.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://cruth.phpnet.org/epfc/caviste/public/index.php/api/';
+
+
+function fetchVins() {
+  return fetch(`${BASE_URL}wines`);
+}
+
+function fetchVinByName(name) {
+    return fetch(`${BASE_URL}wines/${name}`);
+  }
+
+function fetchVinById(id) {
+  return fetch(`${BASE_URL}wines/${id}`);
+}
+
+//  rendre les fonctions accessibles globalement ( sur d autres fichiers)
+window.fetchVins = fetchVins;
+window.fetchVinById = fetchVinById;
+window.fetchVinByName=fetchVinByName;
---
+++ projet/configApi.js
@@ -0,0 +1,7 @@
+export const API_CONFIG = {
+    BASE_URL: 'https://exemple-api.com/',
+    TIMEOUT: 5000,
+    // ... autres configurations spécifiques à l'API
+  };
+
+  //import { API_CONFIG } from './configApi.js';  surr fichier index pour 
